e7d2579011e1c10dedac0776291298489fbcc2ad
Griffen Fargo <griffen@alleyinteractive.com>
2016-12-15 11:51:45 -0500

    updates git-happens presenation

---
+++ GitKraken.md
@@ -0,0 +1,82 @@
+# GitKraken: The Best Git GUI Yet
+
+## What is GitKraken
+
+GitKraken is a cross platform Git graphical user interface written in Node.  It supports Windows, OSX, and Linux
+*( Availabe on Ubuntu LTS 12.04+, Fedora 21+, and Debian 8+ )*.
+
+Unlike [other](https://git-scm.com/downloads/guis) [competitors](https://www.git-tower.com/) with similar feature suites and refinement, **GitKraken is 100% free**.
+
+
+## Workflow Improvments
+
+Check out the [features list](https://www.gitkraken.com/features) on the GitKraken website for an overview of all the current features.
+
+### Better Commits
+
+Forget about `git add .` and start staging individual lines or entire chunks within a single file to create logical commits instead of staging the entire file.
+
+### Longer Commit Messages
+
+GitKraken makes it easy to add detailed comments on each commit, instead of just a single commit message.
+
+### Fuzzy Search
+
+GitKraken features a powerful [fuzzy search](https://blog.axosoft.com/2016/04/28/gitkraken-fuzzy-finder/) tool that allows users to quickly preform various actions like switching repositories, looking at history for a specific file or checking out a new branch.
+
+![GitKraken Fuzzy Search](https://blog.axosoft.com/wp-content/uploads/2016/04/gk-fuzzy-finder.gif)
+
+### Squash Multiple Commits
+
+![](https://blog.axosoft.com/wp-content/uploads/2016/06/gk-pro-tip-squash1.gif)
+
+### Merge How You Want
+
+#### Drag and Drop
+
+### Flexible UI
+
+Because GitKraken is built upon web technologies it excels at adapting to different dimenions.  You can even scale down the interface if you are working on a smaller laptop screen to free up additional screen realestate.
+
+![](https://blog.axosoft.com/wp-content/uploads/2016/06/gk-zoom.gif)
+
+#### Focus on what's important
+
+GitKraken allows you to *solo* individual branches and focus on the commits specific to the branch you are working with.
+
+![](https://blog.axosoft.com/wp-content/uploads/2016/06/soloing-1.gif)
+
+#### Simplify Commit Tree
+
+![](https://blog.axosoft.com/wp-content/uploads/2016/06/gk-graph-resize.gif)
+
+### Built in Merge Tool
+
+In May, GitKraken released a built in [merge tool](https://blog.axosoft.com/2016/05/03/merge-conflict-tool/).  If you prefer another application to resolve conflicts, no problem, GitKraken supports 3rd-party merge tools as well - but for those users who haven't previously installed a merge tool; GitKraken provides a clear path to resolve conflicts inside the app.
+
+![](https://blog.axosoft.com/wp-content/uploads/2016/05/merge-tool-1-copy-1.png)
+
+
+
+
+## Github Interagration
+
+
+### Creation of Pull Requests
+
+
+
+
+## Axosoft is Awesome, like seriously
+
+Axosoft has demonstrated in numerous ways to be a team of concious and caring developers trying to contribute to the space in a positive way.
+
+
+### Active Development
+
+This product is under active development with an active [twitter](https://twitter.com/GitKraken) feed which regularly engages its users.  The team has averaged ~1.25 [minor releases](https://www.gitkraken.com/release-notes) each month since its 1.0.0 candidate back in March 2016.
+
+
+### #ItWasNeverADress
+
+Axosoft actively supports the #ItWasNeverADress invitation with the goal to "...shift perceptions and assumptions about women and the audacious, sensitive, and powerful gestures they make every single day. In science, technology, arts, mathematics, politics, houses of worship, on the streets, and in our homes, insightful women are often uninvited, overlooked, or just plain dismissed. Through storytelling, community building, innovation and creative disruptions, #ItWasNeverADress will foster necessary conversations, vital voices, and images from around the world that honor ALL women. When we see women differently… we see the world differently!" - [source](https://itwasneveradress.com/about/)
--- presentation/index.js
+++ presentation/index.js
@@ -38,6 +38,13 @@ require("spectacle/lib/themes/default/index.css");
 
 const images = {
   city: require("../assets/city.jpg"),
+  badpuns_gif: require("../assets/badpuns.gif"),
+  stageallthethings: require("../assets/stage-all-things.jpg"),
+  gitkraken_logo_small: require("../assets/gitkraken_logo_1.png"),
+  gitkraken_logo: require("../assets/gitkraken_logo_2.png"),
+  classic_gif: require("../assets/classic2.gif"),
+  unwind_gif: require("../assets/unwind.gif"),
+  contender_gif: require("../assets/NewContender.gif"),
   kat: require("../assets/kat.png"),
   logo: require("../assets/formidable-logo.svg"),
   markdown: require("../assets/markdown.png")
@@ -46,7 +53,12 @@ const images = {
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  // primary: "#ff4081"
+  // primary: "#CC3300"
+  // primary: "#0099CC"
+  // primary: "#996699"
+  primary: "#673AB7"
+  // primary: "#3D72A4"
 });
 
 export default class Presentation extends React.Component {
@@ -54,35 +66,229 @@ export default class Presentation extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
-            </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
+
+
+            <Slide transition={["zoom"]} bgColor="primary">
+                <Heading size={1} fit caps lineHeight={1} textColor="black">
+                    Git Happens
+                </Heading>
+                <Heading size={1} fit caps>
+                    CLI Tricks & Workflow Tips to help you
+                </Heading>
+                <Heading size={1} caps textColor="black">
+                    Git Kraken
+                </Heading>
+            </Slide>
+
+            <Slide transition={["slide"]} bgColor="black">
+                <Image src={images.badpuns_gif.replace("/", "")} margin="0px auto 40px" height="320px"/>
+                <Appear fid="1">
+	                <Heading size={1} caps textColor="primary" textFont="primary">
+	                   What to Expect...
+	                </Heading>
+                </Appear>
+            </Slide>
+
+            <Slide transition={["slide"]} bgColor="black" notes="Git Log">
+                <Heading size={1} caps textColor="primary" textFont="primary">
+                    "I need to look back through the commit history"
+                </Heading>
+            </Slide>
+
+            <Slide transition={["zoom", "fade"]} bgColor="primary" notes="Git Log Examples">
+                <Layout>
+                    <Fill align="left">
+                        <Heading size={6} textAlign="left" margin="32px auto" caps textColor="tertiary" textFont="primary">
+                            The CLI method
+                            <Image src={images.classic_gif.replace("/", "")} margin="16px auto"/>
+                        </Heading>
+                    </Fill>
+                    <Fill>
+                        <CodePane
+                            lang="bash"
+                            source={require("raw!../assets/code/git-log.example")}
+                        />
+                    </Fill>
+                </Layout>
+            </Slide>
+
+            <Slide transition={["zoom", "fade"]} bgColor="primary" notes="Git Log Graph">
+                <CodePane
+                    lang="bash"
+                    margin="20px auto"
+                    source={require("raw!../assets/code/git-log-graph.example")}
+                />
+            </Slide>
+
+            <Slide transition={["slide"]} bgColor="black">
+                <Heading size={1} caps fit textColor="primary" textFont="primary">
+                    A few Advantages of a GUI
+                </Heading>
+                <Layout>
+                    <Fill>
+                        <Image transition={["fade", "zoom"]} src={images.contender_gif.replace("/", "")} margin="40px auto 0" height="280"/>
+                    </Fill>
+                    <Fill>
+                        <Image transition={["fade", "zoom"]} src={images.gitkraken_logo.replace("/", "")} margin="40px auto 0" height="280"/>
+                    </Fill>
+                </Layout>
+            </Slide>
+
+
+
+
+            <Slide transition={["zoom", "fade"]} bgColor="black" notes="Breaking Apart Work">
+                <Image src={images.unwind_gif.replace("/", "")} margin="0px auto 40px" height="275px"/>
+                <Heading size={4} caps textColor="primary" textFont="primary">
+                    "I was caught in a coding trance and now have a 16 files to stage..."
+                </Heading>
+            </Slide>
+
+            <Slide transition={["zoom", "fade"]} bgColor="black" notes="Breaking Apart Work">
+                <Image src={images.stageallthethings.replace("/", "")} margin="0px auto 40px" height="320px"/>
+                <CodePane
+                    margin="20px auto"
+                    source={require("raw!../assets/code/git-add.example")}
+                />
+                <Heading size={1} caps textColor="primary" textFont="primary">
+                    🤔
+                </Heading>
+            </Slide>
+
+            <Slide transition={["zoom", "fade"]} bgColor="primary" notes="Git Log Examples">
+                <Layout>
+                    <Fill>
+                        <CodePane
+                            lang="bash"
+                            source={require("raw!../assets/code/git-add-patch.example")}
+                        />
+                    </Fill>
+                    <Fill align="left">
+                        <Heading textSize={36} caps textColor="white" textAlign="right" padding="0 0 0 20px" textFont="primary">
+                            Git patch steps through each change individually, allowing you to stage only what is important.
+                        </Heading>
+
+                    </Fill>
+                </Layout>
+            </Slide>
+
+            <Slide transition={["zoom", "fade"]} bgColor="primary" notes="Git Log Examples">
+                <Layout>
+                    <Fill>
+                        <CodePane
+                            lang="bash"
+                            source={require("raw!../assets/code/git-add-interactive.example")}
+                        />
+                    </Fill>
+                    <Fill textAlign="right">
+                        <Heading textSize={36} caps textColor="white" textAlign="right" padding="0 0 0 20px" textFont="primary">
+                            --interactive (or -i) is the big brother of --patch.  patch only lets you decide about the individual hunks in files whereas interactive enters the interactive mode.
+                        </Heading>
+                        <Link textAlign="right" textColor="white" href="http://nuclearsquid.com/writings/git-add/">Nuclear Squid</Link>
+                    </Fill>
+                </Layout>
+            </Slide>
+
+
+
+
+
+
+            <Slide transition={["zoom", "fade"]} bgColor="black" notes="Merge Conflicts">
+                <Heading size={1} caps textColor="primary" textFont="primary">
+                    "Always running into conflicts when I'm trying to merge my work..."
+                </Heading>
+            </Slide>
+
+            <Slide transition={["spin", "slide"]} bgColor="black">
+                <Heading size={6} caps textColor="primary" textFont="primary">
+                    1. take time to understand what is happening.
+                </Heading>
+
+                <Heading size={6} caps textColor="primary" textFont="primary">
+                    2. use the tool you feel most comfortable with.
+                </Heading>
+
+                <Heading margin="0 auto 32px" size={5} caps textColor="primary" textFont="primary">
+                    3. when in doubt recompile.
+                </Heading>
+
+
+                <Layout>
+                    <Fill>
+                        <CodePane
+                            lang="bash"
+                            margin="20px auto"
+                            source={require("raw!../assets/code/git-log-merge.example")}
+                        />
+                    </Fill>
+                    <Fill>
+                        <BlockQuote>
+                            <Quote textSize={24} fit caps textColor="white">
+                                Displays all of the commits that touched that file in between the common ancestor and the two heads you are merging.
+                            </Quote>
+                            <Cite textColor="white" textSize={14}>
+                                <Link textColor="white" href="http://stackoverflow.com/a/7589612/1731759">Peter Mortensen @ S.O.</Link>
+                            </Cite>
+                        </BlockQuote>
+                    </Fill>
+                </Layout>
+            </Slide>
+
+
+
+
+            <Slide transition={["zoom", "fade"]} bgColor="black" notes="Creating a PR">
+                <Heading size={1} caps textColor="primary" textFont="primary">
+                    "I want to create a new PR"
+                </Heading>
+            </Slide>
+
+            <Slide transition={["zoom", "fade"]} bgColor="primary" notes="Git Log Graph">
+                <Layout>
+                    <Fill>
+                        <Heading size={6} textAlign="left" caps padding="0 24px 0 0" textColor="tertiary" textFont="primary">
+                            open a compare against the current branch on github
+                        </Heading>
+                    </Fill>
+                    <Fill>
+                        <CodePane
+                            lang="bash"
+                            source={require("raw!../assets/code/git-cpr.example")}
+                        />
+                    </Fill>
+                </Layout>
+            </Slide>
+
+
+
+
+
+
+            <Slide transition={["zoom", "fade"]} bgColor="black" notes="Multiple Remotes">
+                <Heading size={1} caps textColor="primary" textFont="primary">
+				    "Interacting with multiple remotes"
+                </Heading>
+            </Slide>
+
+            <Slide transition={["spin", "slide"]} bgColor="tertiary">
+                <Heading size={1} caps textColor="primary" textFont="primary">
+                    Additional Resources
+                </Heading>
+                <List>
+                    <ListItem><Link href="https://githowto.com">GitHowTo.com</Link></ListItem>
+                    <ListItem><Link href="http://ohshitgit.com/">OhSh*tGit.com</Link></ListItem>
+                    <ListItem><Link href="https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh">oh-my-zsh git plugin</Link></ListItem>
+                    <ListItem><Link href="http://codetunnel.io/tips-for-working-with-git/">CodeTunnel: Tips for Working with Git</Link></ListItem>
+                    <ListItem><Link href="https://github.com/git-tips/tips">@git-tips: tips</Link></ListItem>
+                    <ListItem><Link href="https://github.com/arslanbilal/git-cheat-sheet">@arslanbilal: git cheat sheet</Link></ListItem>
+                </List>
+            </Slide>
+
+
+
+
+          <Slide transition={["slide"]}  bgDarken={0.75}>
             <Appear fid="1">
               <Heading size={1} caps fit textColor="primary">
                 Full Width
@@ -99,6 +305,8 @@ export default class Presentation extends React.Component {
               </Heading>
             </Appear>
           </Slide>
+
+
           <Slide transition={["zoom", "fade"]} bgColor="primary">
             <Heading caps fit>Flexible Layouts</Heading>
             <Layout>
@@ -114,12 +322,16 @@ export default class Presentation extends React.Component {
               </Fill>
             </Layout>
           </Slide>
+
+
           <Slide transition={["slide"]} bgColor="black">
             <BlockQuote>
               <Quote>Wonderfully formatted quotes</Quote>
               <Cite>Ken Wheeler</Cite>
             </BlockQuote>
           </Slide>
+
+
           <Slide transition={["spin", "zoom"]} bgColor="tertiary">
             <Heading caps fit size={1} textColor="primary">
               Inline Markdown
@@ -135,6 +347,8 @@ You can write inline images, [Markdown Links](http://commonmark.org), paragraph 
               `}
             </Markdown>
           </Slide>
+
+
           <Slide transition={["slide", "spin"]} bgColor="primary">
             <Heading caps fit size={1} textColor="tertiary">
               Smooth
@@ -143,6 +357,8 @@ You can write inline images, [Markdown Links](http://commonmark.org), paragraph 
               Combinable Transitions
             </Heading>
           </Slide>
+
+
           <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
             <List>
               <Appear><ListItem>Inline style based theme system</ListItem></Appear>
@@ -153,18 +369,24 @@ You can write inline images, [Markdown Links](http://commonmark.org), paragraph 
               <Appear><ListItem>And...</ListItem></Appear>
             </List>
           </Slide>
+
+
           <Slide transition={["slide"]} bgColor="primary">
             <Heading size={1} caps fit textColor="tertiary">
               Your presentations are interactive
             </Heading>
             <Interactive/>
           </Slide>
+
+
           <Slide transition={["spin", "slide"]} bgColor="tertiary">
             <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
               Made with love in Seattle by
             </Heading>
             <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
           </Slide>
+
+
         </Deck>
       </Spectacle>
     );
